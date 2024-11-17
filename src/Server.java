8aa4554f94c2074e5339c923bbb023a9f43db02e
Gaurgle <justwannagroove@gmail.com>
2024-11-17 23:18:42 +0100

    server init

---
+++ src/Server.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public Server () {
+        int port = 1234;
+
+        try (ServerSocket serverSock = new ServerSocket(port)) {
+            System.out.println("Server started; port " + port);
+
+            while (true) {
+                try (
+                    Socket socket = serverSock.accept();
+                    InputStreamReader inputReader = new InputStreamReader(new BufferedInputStream(socket.getInputStream()));
+                    OutputStreamWriter outputWriter = new OutputStreamWriter(new BufferedOutputStream(socket.getOutputStream()));
+                    BufferedReader bufferedReader = new BufferedReader(inputReader);
+                    BufferedWriter bufferedWriter = new BufferedWriter(outputWriter))
+                    {
+
+                    System.out.println("Client connected" + socket.getRemoteSocketAddress());
+
+                    String messageToClient = "Connected to server";
+                    bufferedWriter.write("server: " +messageToClient);
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+
+                    String messageFromClient;
+                    while ((messageFromClient = bufferedReader.readLine()) != null) {
+                        System.out.println("client: " + messageFromClient);
+
+                        if (messageFromClient.equalsIgnoreCase("EXIT")) {
+                            break;
+                        }
+                    }
+                    System.out.println("client disconnected");
+
+                } catch (IOException e) {
+                    System.err.println("Error handlining client connection" + e.getMessage());;
+                }
+        }
+
+        } catch (IOException e) {
+            System.err.println("Problem with server" +e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+       new Server();
+    }
+}
