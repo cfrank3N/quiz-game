d21bc75b08d6b32e50c919f084643560c3bd63d1
Gaurgle <justwannagroove@gmail.com>
2024-11-18 14:47:45 +0100

    mutlithreaded server

---
+++ src/ClientHandler.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private Socket socket;
+
+    public ClientHandler(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try (
+            InputStreamReader inputReader = new InputStreamReader(new BufferedInputStream(socket.getInputStream()));
+            OutputStreamWriter outputWriter = new OutputStreamWriter(new BufferedOutputStream(socket.getOutputStream()));
+            BufferedReader bufferedReader = new BufferedReader(inputReader);
+            BufferedWriter bufferedWriter = new BufferedWriter(outputWriter)
+        ) {
+            String messageToClient = "Connected to server";
+            bufferedWriter.write("server: " + messageToClient);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            String messageFromClient;
+            while ((messageFromClient = bufferedReader.readLine()) != null) {
+                System.out.println("client: " + messageFromClient);
+
+                if (messageFromClient.equalsIgnoreCase("EXIT")) {
+                    bufferedWriter.write("server: Goodbye.");
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    break;
+
+                }
+
+                bufferedWriter.write("server: received" + messageFromClient);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+
+            System.out.println("client disconnected");
+
+        } catch (IOException e) {
+            System.err.println("Error handling client connection" + e.getMessage());
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                System.err.println("Error closing socket" + e.getMessage());
+            }
+        }
+    }
+}
--- src/Server.java
+++ src/Server.java
@@ -11,38 +11,30 @@ public class Server {
             System.out.println("Server started; port " + port);
 
             while (true) {
-                try (
-                    Socket socket = serverSock.accept();
-                    InputStreamReader inputReader = new InputStreamReader(new BufferedInputStream(socket.getInputStream()));
-                    OutputStreamWriter outputWriter = new OutputStreamWriter(new BufferedOutputStream(socket.getOutputStream()));
-                    BufferedReader bufferedReader = new BufferedReader(inputReader);
-                    BufferedWriter bufferedWriter = new BufferedWriter(outputWriter))
-                    {
+                Socket p1sock = serverSock.accept();
+                System.out.println("Player1 connected" + p1sock.getRemoteSocketAddress());
+                Socket p2sock = serverSock.accept();
+                System.out.println("Player2 connected" + p2sock.getRemoteSocketAddress());
 
-                    System.out.println("Client connected" + socket.getRemoteSocketAddress());
+                ObjectOutputStream p1Out = new ObjectOutputStream(p1sock.getOutputStream());
+                ObjectInputStream p1In = new ObjectInputStream(p2sock.getInputStream());
+                ClientHandler p1Handler = new ClientHandler(p1sock);
 
-                    String messageToClient = "Connected to server";
-                    bufferedWriter.write("server: " +messageToClient);
-                    bufferedWriter.newLine();
-                    bufferedWriter.flush();
+                ObjectOutputStream p2Out = new ObjectOutputStream(p2sock.getOutputStream());
+                ObjectInputStream p2In = new ObjectInputStream(p2sock.getInputStream());
+                ClientHandler p2Handler = new ClientHandler(p2sock);
 
-                    String messageFromClient;
-                    while ((messageFromClient = bufferedReader.readLine()) != null) {
-                        System.out.println("client: " + messageFromClient);
+                Thread p1Thread = new Thread(p1Handler);
+                Thread p2Thread = new Thread(p2Handler);
 
-                        if (messageFromClient.equalsIgnoreCase("EXIT")) {
-                            break;
-                        }
-                    }
-                    System.out.println("client disconnected");
-
-                } catch (IOException e) {
-                    System.err.println("Error handling client connection" + e.getMessage());;
-                }
-        }
-
+                p1Thread.start();
+                Thread.sleep(100);
+                p2Thread.start();
+            }
         } catch (IOException e) {
-            System.err.println("Problem with server" +e.getMessage());
+            System.err.println("Problem with server" + e.getMessage());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 
