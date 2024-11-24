15728d0e31d2a09e47f46f9386f8cc7aff285fa3
AdamRitzler <adam.ritzler@gmail.com>
2024-11-24 15:39:43 +0100

    skapat subject och knappar för att kunna välja.

--- src/serverSide/GameLook.java
+++ src/serverSide/GameLook.java
@@ -9,6 +9,15 @@ public class GameLook extends JFrame implements ActionListener {
     //JFrame for the game
     JFrame gameFrame = new JFrame();
     JPanel firstPanel = new JPanel();
+    JPanel buttonPanel = new JPanel();
+    JPanel categoryPanel = new JPanel();
+    JLabel chooseCategory = new JLabel("Choose your category");
+    JButton subject1 = new JButton("Subject 1");
+    JButton subject2 = new JButton("Subject 2");
+    JButton subject3 = new JButton("Subject 3");
+    JButton subject4 = new JButton("subject 4");
+    JButton emtybutton = new JButton("");
+
 
     //JFrame for the "lobby"
     JPanel headPanel = new JPanel();
@@ -47,7 +56,8 @@ public class GameLook extends JFrame implements ActionListener {
 
         // Anpassa komponenter
         picture.setIcon(profile);
-        userName.setFont(new Font("Arial", Font.BOLD, 16));
+        userName.setFont(new Font("Arial", Font.BOLD, 20));
+        waitingPlayer.setFont(new Font("Arial", Font.BOLD, 20));
         score.setFont(new Font("Arial", Font.BOLD, 16));
         newGame.setFont(new Font("Arial", Font.BOLD, 16));
         userName.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -81,15 +91,46 @@ public class GameLook extends JFrame implements ActionListener {
         //gör den första Frame osynlig
         this.setVisible(false);
 
-        gameFrame.setSize(500, 800);
+        //Startar en ny frame för spelet.
+        gameFrame.setSize(500, 650);
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameFrame.setTitle("the quiz game");
         gameFrame.setLocationRelativeTo(null);
 
         firstPanel.setLayout(new BorderLayout());
+        buttonPanel.setLayout(new FlowLayout());
+        categoryPanel.setLayout(new BorderLayout());
 
         gameFrame.add(firstPanel);
+        firstPanel.add(buttonPanel, BorderLayout.CENTER);
+        firstPanel.add(categoryPanel, BorderLayout.NORTH);
 
+        categoryPanel.add(chooseCategory, BorderLayout.CENTER);
+        buttonPanel.add(emtybutton);
+
+        buttonPanel.add(subject1);
+        buttonPanel.add(subject2);
+        buttonPanel.add(subject3);
+        buttonPanel.add(subject4);
+
+
+        emtybutton.setContentAreaFilled(false);
+        emtybutton.setBorderPainted(false);
+
+
+        buttonPanel.setBackground(Color.CYAN);
+        categoryPanel.setBackground(Color.red);
+        categoryPanel.setPreferredSize(new Dimension(500, 200));
+        chooseCategory.setFont(new Font("Arial", Font.BOLD, 16));
+        chooseCategory.setHorizontalAlignment(SwingConstants.CENTER);
+        chooseCategory.setVerticalAlignment(SwingConstants.CENTER);
+
+
+        emtybutton.setPreferredSize(new Dimension(500, 50));
+        subject1.setPreferredSize(new Dimension(225, 150));
+        subject2.setPreferredSize(new Dimension(225, 150));
+        subject3.setPreferredSize(new Dimension(225, 150));
+        subject4.setPreferredSize(new Dimension(225, 150));
 
         gameFrame.setVisible(true);
     }
@@ -108,7 +149,7 @@ public class GameLook extends JFrame implements ActionListener {
             GameStart();
         }
         if (e.getSource() == score) {
-            JOptionPane.showMessageDialog(this, "the scoreboard does not exits right now");
+            JOptionPane.showMessageDialog(this, "The scoreboard does not exits right now");
         }
     }
 }
