ae0c9bde7966e1a2676e7c5bffcbb2e8d75342a7
Havrem <144966173+Havrem@users.noreply.github.com>
2024-11-25 12:01:51 +0100

    Add questions

--- src/database/TestingEnvironment.java
+++ src/database/TestingEnvironment.java
@@ -13,12 +13,17 @@ public class TestingEnvironment {
         UserRepository ur = new UserRepository();
         List<Question> questions = new ArrayList<>();
 
-        questions.add(new Question("5", "15", "50", "25", ESubject.SUBJECT1, "What is 5 times 5?"));
-        questions.add(new Question("13", "2", "0", "20", ESubject.SUBJECT1, "What is 10 times 2?"));
-
-        for (Question q : questions) {
-            qr.save(q);
-        }
+//        questions.add(new Question("5", "15", "50", "25", ESubject.SUBJECT1, "What is 5 times 5?"));
+//        questions.add(new Question("13", "2", "0", "20", ESubject.SUBJECT1, "What is 10 times 2?"));
+//        questions.add(new Question("13", "2", "0", "20", ESubject.SUBJECT1, "What is 10 times 2?"));
+//        questions.add(new Question("14", "2", "0", "20", ESubject.SUBJECT2, "What is the capital of France?"));
+//        questions.add(new Question("15", "2", "0", "20", ESubject.SUBJECT3, "What is the square root of 16?"));
+//        questions.add(new Question("16", "2", "0", "20", ESubject.SUBJECT4, "Who wrote 'Romeo and Juliet'?"));
+//        questions.add(new Question("17", "2", "0", "20", ESubject.SUBJECT5, "How many continents are there on Earth?"));
+//
+//        for (Question q : questions) {
+//            qr.save(q);
+//        }
 
         List<Question> retrieved = qr.retrieveAll();
         for (Question q : retrieved) {
